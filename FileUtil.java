377fbc18b10ce9b66aae679584f6287f2be4502c
ruochenxing <sherlock_65535@qq.com>
2014-11-06 11:36:03 +0800

    Update FileUtil.java

--- FileUtil.java
+++ FileUtil.java
@@ -1,7 +1,9 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
 public class FileUtil {
 	/**
 	 * 删除某个文件夹下的所有文件夹和文件
@@ -37,5 +39,36 @@ public class FileUtil {
 			System.out.println("deletefile() Exception:" + e.getMessage());
 		}
 		return true;
+	}
+	/**
+	 * 复制文件
+	 * */
+	public static void fileChannelCopy(String s, String t) {
+		File file=new File(s);
+		if(!file.exists()){
+			return;
+		}
+		FileInputStream fi = null;
+		FileOutputStream fo = null;
+		FileChannel in = null;
+		FileChannel out = null;
+		try {
+			fi = new FileInputStream(s);
+			fo = new FileOutputStream(t);
+			in = fi.getChannel();// 得到对应的文件通道
+			out = fo.getChannel();// 得到对应的文件通道
+			in.transferTo(0, in.size(), out);// 连接两个通道，并且从in通道读取，然后写入out通道
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				fi.close();
+				in.close();
+				fo.close();
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
