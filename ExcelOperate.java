c5030a2322e10f1c986c75ee14bc50eeabab95e9
ruochenxing <sherlock_65535@qq.com>
2015-01-07 21:04:49 +0800

    get excel info

---
+++ ExcelOperate.java
@@ -0,0 +1,195 @@
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFDateUtil;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+
+public class ExcelOperate {
+	private static Map<String, Integer> map = new HashMap<String, Integer>();
+
+	public static void main(String[] args) throws Exception {
+		File file = new File(
+				"C:\\Users\\Administrator\\Desktop\\2015学生论文选题.xls");
+		String[][] result = getData(file, 2);
+		int rowLength = result.length;
+		for (int i = 0; i < rowLength; i++) {
+			put(result[i][5]);
+		}
+		map=sortMap(map);
+		workByEntry(map);
+	}
+
+	public static void workByEntry(Map<String, Integer> map) {
+		Set<Map.Entry<String, Integer>> set = map.entrySet();
+		for (Iterator<Map.Entry<String, Integer>> it = set.iterator(); it
+				.hasNext();) {
+			Map.Entry<String, Integer> entry = (Map.Entry<String, Integer>) it
+					.next();
+			System.out.println(entry.getKey() + "--->" + entry.getValue());
+		}
+	}
+
+	public static Map sortMap(Map oldMap) {
+		ArrayList<Map.Entry<String, Integer>> list = new ArrayList<Map.Entry<String, Integer>>(
+				oldMap.entrySet());
+		Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+			public int compare(Entry<String, Integer> o1,
+					Entry<String, Integer> o2) {
+				return o1.getValue() - o2.getValue();
+			}
+		});
+		Map newMap = new LinkedHashMap();
+		for (int i = 0; i < list.size(); i++) {
+			newMap.put(list.get(i).getKey(), list.get(i).getValue());
+		}
+		return newMap;
+	}
+
+	private static void put(String key) {
+		if (map.containsKey(key)) {
+			map.put(key, map.get(key) + 1);
+		} else {
+			map.put(key, 1);
+		}
+	}
+
+	/**
+	 * 读取Excel的内容，第一维数组存储的是一行中格列的值，二维数组存储的是多少个行
+	 * 
+	 * @param file
+	 *            读取数据的源Excel
+	 * @param ignoreRows
+	 *            读取数据忽略的行数，比喻行头不需要读入 忽略的行数为1
+	 * @return 读出的Excel中数据的内容
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	public static String[][] getData(File file, int ignoreRows)
+			throws FileNotFoundException, IOException {
+		List<String[]> result = new ArrayList<String[]>();
+		int rowSize = 0;
+		BufferedInputStream in = new BufferedInputStream(new FileInputStream(
+				file));
+		POIFSFileSystem fs = new POIFSFileSystem(in);
+		HSSFWorkbook wb = new HSSFWorkbook(fs);
+		HSSFCell cell = null;
+		for (int sheetIndex = 0; sheetIndex < wb.getNumberOfSheets(); sheetIndex++) {
+			HSSFSheet st = wb.getSheetAt(sheetIndex);
+			for (int rowIndex = ignoreRows; rowIndex <= st.getLastRowNum(); rowIndex++) {
+				HSSFRow row = st.getRow(rowIndex);
+				if (row == null) {
+					continue;
+				}
+				int tempRowSize = row.getLastCellNum() + 1;
+				if (tempRowSize > rowSize) {
+					rowSize = tempRowSize;
+				}
+				String[] values = new String[rowSize];
+				Arrays.fill(values, "");
+				boolean hasValue = false;
+				for (short columnIndex = 0; columnIndex <= row.getLastCellNum(); columnIndex++) {
+					String value = "";
+					cell = row.getCell(columnIndex);
+					if (cell != null) {
+						// 注意：一定要设成这个，否则可能会出现乱码
+						cell.setEncoding(HSSFCell.ENCODING_UTF_16);
+						switch (cell.getCellType()) {
+						case HSSFCell.CELL_TYPE_STRING:
+							value = cell.getStringCellValue();
+							break;
+						case HSSFCell.CELL_TYPE_NUMERIC:
+							if (HSSFDateUtil.isCellDateFormatted(cell)) {
+								Date date = cell.getDateCellValue();
+								if (date != null) {
+									value = new SimpleDateFormat("yyyy-MM-dd")
+											.format(date);
+								} else {
+									value = "";
+								}
+							} else {
+								value = new DecimalFormat("0").format(cell
+										.getNumericCellValue());
+							}
+							break;
+						case HSSFCell.CELL_TYPE_FORMULA:
+							// 导入时如果为公式生成的数据则无值
+							if (!cell.getStringCellValue().equals("")) {
+								value = cell.getStringCellValue();
+							} else {
+								value = cell.getNumericCellValue() + "";
+							}
+							break;
+						case HSSFCell.CELL_TYPE_BLANK:
+							break;
+						case HSSFCell.CELL_TYPE_ERROR:
+							value = "";
+							break;
+						case HSSFCell.CELL_TYPE_BOOLEAN:
+							value = (cell.getBooleanCellValue() == true ? "Y"
+									: "N");
+							break;
+						default:
+							value = "";
+						}
+					}
+					if (columnIndex == 0 && value.trim().equals("")) {
+						break;
+					}
+					values[columnIndex] = rightTrim(value);
+					hasValue = true;
+				}
+				if (hasValue) {
+					result.add(values);
+				}
+			}
+		}
+		in.close();
+		String[][] returnArray = new String[result.size()][rowSize];
+		for (int i = 0; i < returnArray.length; i++) {
+			returnArray[i] = (String[]) result.get(i);
+		}
+		return returnArray;
+	}
+
+	/**
+	 * 去掉字符串右边的空格
+	 * 
+	 * @param str
+	 *            要处理的字符串
+	 * @return 处理后的字符串
+	 */
+	public static String rightTrim(String str) {
+		if (str == null) {
+			return "";
+		}
+		int length = str.length();
+		for (int i = length - 1; i >= 0; i--) {
+			if (str.charAt(i) != 0x20) {
+				break;
+			}
+			length--;
+		}
+		return str.substring(0, length);
+	}
+}
